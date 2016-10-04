8b4a4e3bc5f1af5d7e4720cc5142bf1c99085cd3
yeswanth82 <yeswanth82@gmail.com>
2016-10-03 21:56:00 -0400

    ACTIVE,INROUTE working but very slow

--- PushtoGo/Controllers/OrderStatusController.cs
+++ PushtoGo/Controllers/OrderStatusController.cs
@@ -25,25 +25,30 @@ namespace PushtoGo.Controllers
         }
 
         public ActionResult GetPage()
+        {         
+            return View("Index");
+        }
+
+        public ActionResult ActiveOrders(string categoryId)
         {
             OrderInformation orderInformation = new OrderInformation();
             orderInformation.lstorder = new List<string>();
-
-            ReadActiveOrders();
-            foreach (var order in activeOrder)
-              orderInformation.lstorder.Add(order);
-           
-            orderInformation.userid = "For Display";
             
-            return View("Index",orderInformation);
-        }
 
-        public void ReadActiveOrders()
+          //  ReadOrders(categoryId);
+           // foreach (var order in activeOrder)
+             //   orderInformation.lstorder.Add(order);
+
+            orderInformation.lstorder.Add(DateTime.Now.ToString()+ DateTime.Now.ToString()+ DateTime.Now.ToString());
+
+            return View(orderInformation);
+        }
+        public void ReadOrders(string orderstatus)
         {
             activeOrder = new List<string>();
             try
             {
-                Console.WriteLine("Read Password");
+                
                 dynamoClient = new AmazonDynamoDBClient(new BasicAWSCredentials("AKIAJOMD6KZKIDIWR3GA",
                     "oJNTQDc647jLVVNC/fgYbIwzf1GnznroAeIE3gaN"),
                     RegionEndpoint.GetBySystemName("us-east-1")
@@ -51,7 +56,7 @@ namespace PushtoGo.Controllers
                 Table table = Table.LoadTable(dynamoClient, "ORDER_TABLE0");
 
                 ScanFilter scanFilter = new ScanFilter();
-                scanFilter.AddCondition("STATUS", ScanOperator.Equal, "ACTIVE");
+                scanFilter.AddCondition("STATUS", ScanOperator.Equal, orderstatus);
                 Search search = table.Scan(scanFilter);
 
                 List<Document> documentList = new List<Document>();
@@ -62,9 +67,10 @@ namespace PushtoGo.Controllers
 
                     foreach (var document in documentList)
                     {
-                        if(document["STATUS"].ToString() =="ACTIVE" && document["HOTEL_ID"].ToString() == Session["Hotleid"].ToString())
+                        if(document["STATUS"].ToString() == orderstatus && document["HOTEL_ID"].ToString() == Session["Hotleid"].ToString())
                         { 
-                        activeOrder.Add(document["ROOM_NO"].ToString()+" "+ document["ORDER_ID"].ToString());
+                        activeOrder.Add(document["ROOM_NO"].ToString()+" "+ document["ORDER_ID"].ToString() +" " + document["CREATE_TIMESTAMP"].ToString());
+                            
                         }
                     }
                        
@@ -74,6 +80,65 @@ namespace PushtoGo.Controllers
             catch (Exception exception)
             {
 
+            }
+        }
+
+        public void UpdateOrders(string orderid, string status)
+        {
+            string timestamp = DateTime.Now.ToString();
+            switch(status)
+                {
+                case "ACTIVE":
+                    status = "IN-ROUTE";
+                    break;
+                case "IN-ROUTE":
+                    status = "COMPLETED";
+                    break;
+                //case "COMPLETED":
+                //    status = "CLOSED";
+                //    break;
+
+            }
+            try
+            {
+                dynamoClient = new AmazonDynamoDBClient(new BasicAWSCredentials("AKIAJOMD6KZKIDIWR3GA",
+                    "oJNTQDc647jLVVNC/fgYbIwzf1GnznroAeIE3gaN"),
+                    RegionEndpoint.GetBySystemName("us-east-1")
+                    );
+                Table table = Table.LoadTable(dynamoClient, "ORDER_TABLE0");
+
+                ScanFilter scanFilter = new ScanFilter();
+                scanFilter.AddCondition("HOTEL_ID", ScanOperator.NotEqual, "0");
+
+                Search search = table.Scan(scanFilter);
+                List<Document> documentList = new List<Document>();
+                do
+                {
+                    documentList = search.GetNextSet();
+                    foreach (var document in documentList)
+                    {
+                        document["STATUS"] = status;
+                        document["CREATE_TIMESTAMP"] = timestamp;
+
+                        if (document["ORDER_ID"].AsPrimitive().Value.ToString() == orderid.ToString())
+                        {
+                            Expression expr = new Expression();
+                            expr.ExpressionStatement = "ORDER_ID = :val";
+                            expr.ExpressionAttributeValues[":val"] = orderid;
+                            // Optional parameters.
+                            UpdateItemOperationConfig config = new UpdateItemOperationConfig
+                            {
+                                ConditionalExpression = expr,
+                                ReturnValues = ReturnValues.AllNewAttributes
+                            };
+                            // table.TryUpdateItem(document, config);
+                             table.UpdateItem(document, config);
+                        }
+                    }
+                } while (!search.IsDone);
+            }
+            catch (Exception exception)
+            {
             }
         }
     }
